ec943215a86285fbef2f1f85d40b5fa3bc83ec51
Ismail Baris <ismail.93.baris@gmail.com>
2025-12-17 04:08:04 +0100

    changed: background-object -- comments fixed

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,29 +1,29 @@
 /**
- * Repräsentiert ein statisches Hintergrundobjekt im Level,
- * das sich mit der Kamera verschiebt (Parallax/Scrolling).
+ * Represents a static background object in the level
+ * that moves with the camera (parallax/scrolling).
  * @extends MovableObject
  */
 class BackgroundObject extends MovableObject {
     /**
-     * Breite des Hintergrundbildes in Pixeln.
+     * Width of the background image in pixels.
      * @type {number}
      */
     width = 720;
 
     /**
-     * Höhe des Hintergrundbildes in Pixeln.
+     * Height of the background image in pixels.
      * @type {number}
      */
     height = 480;
 
     /**
-     * Erstellt ein neues Hintergrundobjekt.
-     * @param {string} imagePath - Pfad zum Hintergrundbild.
-     * @param {number} x - X-Position im Level.
+     * Creates a new background object.
+     * @param {string} imagePath - Path to the background image.
+     * @param {number} x - X position within the level.
      */
     constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
-        this.y = 480 - this.height; // Hintergrund unten abschließen
+        this.y = 480 - this.height; // Align background to the bottom
     }
 }
